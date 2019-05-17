cd34c60b05f5a875fdcd0340e5a59ad1c4a658ce
Mitchel van Eijgen <mvaneijgen@gmail.com>
2019-05-17 17:00:05 +0200

    Add docs foler

---
+++ docs/_nuxt/33ea0d2e42ee82f6bb2f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{202:function(t,n,e){"use strict";e.r(n);var r={data:function(){return{}}},c=e(7),component=Object(c.a)(r,function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._v("about")])},[],!1,null,null,null);n.default=component.exports}}]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: "/wish-list/",
+          base: "/mijn-tic-en-ik/",
         },
       }
     : {};
